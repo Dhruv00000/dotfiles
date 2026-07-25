0311b5f5dd2564e519cfd12f752f10b223c83eae
Dhruv <dhruvmitna9@gmail.com>
2026-07-25 12:25:45 +0530

    bugfix

--- scripts/wallpaper_and_colour_scheme.sh
+++ scripts/wallpaper_and_colour_scheme.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cd ${HOME}/Pictures/wallpapers
-RAND=$(($(date +%N) % $(ls -h | wc -l) + 1))
-IMG=$(ls -h | awk -v r=$RAND 'BEGIN{FS = "\n"; RS = ""} {print $r}')
-swaybg --image ${HOME}/Pictures/wallpapers/$IMG &
+WALLPAPER_FOLDER="${HOME}/Pictures/wallpapers"
 
-iris ${HOME}/Pictures/wallpapers/$IMG --glass --dark 1 && mv ~/.cache/iris/colors-kitty.conf ~/.config/kitty/colors.conf && mv ~/.cache/iris/rofi.rasi ~/.config/rofi/colors/rofi.rasi && mv ~/.cache/iris/quickshell.qml ~/.config/quickshell/Colors.qml && mv ~/.cache/iris/starship.toml ~/.config/starship.toml && mv ~/.cache/iris/style.css ~/.config/wlogout/style.css
+cd "$WALLPAPER_FOLDER"
+IMG=$(ls | shuf -n 1)
+swaybg --image "$WALLPAPER_FOLDER/$IMG" & iris "$WALLPAPER_FOLDER/$IMG" --glass --dark 1 && mv ~/.cache/iris/colors-kitty.conf ~/.config/kitty/colors.conf && mv ~/.cache/iris/rofi.rasi ~/.config/rofi/colors/rofi.rasi && mv ~/.cache/iris/quickshell.qml ~/.config/quickshell/Colors.qml && mv ~/.cache/iris/starship.toml ~/.config/starship.toml && mv ~/.cache/iris/style.css ~/.config/wlogout/style.css
 
 exit 0
