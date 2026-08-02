2b8d56d93ec1ebedccf188ce45f6f9df8f663169
Dhruv <dhruvmitna9@gmail.com>
2026-08-02 18:51:57 +0530

    Added extended screenshot functionality

--- scripts/screenshot_capturer.sh
+++ scripts/screenshot_capturer.sh
@@ -2,7 +2,7 @@ FILE="$HOME/Pictures/Screenshots/$(date +'%d_%m_%Y__%H_%M_%S').png"
 grim -g "$(slurp)" "$FILE" || exit 0
 
 
-ACTION=$(notify-send "Screenshot Captured" "Copied to clipboard" --icon="$FILE" --action="edit=Annotate" --action="lens=Google Lens" --expire-time=5000)
+ACTION=$(notify-send "Screenshot Captured" "Copied to clipboard" --icon="$FILE" --action="edit=annotate" --action="lens=search" --action="scroll=extend" --expire-time=5000)
 case "$ACTION" in
     "edit")
         satty --filename "$FILE" --output-filename "$FILE" --disable-notifications
@@ -10,6 +10,7 @@ case "$ACTION" in
     "lens")
 
         IMAGE_URL=$(curl -s -F "reqtype=fileupload" -F "time=1h" -F "fileToUpload=@$FILE" https://litterbox.catbox.moe/resources/internals/api.php) # First, the file is uploaded online so that google lens has a link to the image
+        # TODO: replace this website with file uploading to your own server
 
         if [ -n "$IMAGE_URL" ]; then
             zen-browser "https://lens.google.com/uploadbyurl?url=${IMAGE_URL}"
@@ -17,6 +18,10 @@ case "$ACTION" in
         else
             notify-send "Google Lens Error" "Failed to upload image for search."
         fi
+        ;;
+    "scroll")
+        notify-send "Scrolling Screenshot" "Select region and start scrolling" --expire-time=2000
+        wayscrollshot --output "$FILE"
         ;;
 esac
 
